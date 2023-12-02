68b3996437bff45bcaca579fdac2d6d5c5ab11a8
VladyslavKosarenko <131978971+VladyslavKosarenko@users.noreply.github.com>
2023-12-02 12:20:40 +0200

    finish

--- src/components/CountryInfo/CountryInfo.jsx
+++ src/components/CountryInfo/CountryInfo.jsx
@@ -17,27 +17,27 @@ export const CountryInfo = ({
   languages = [],
   population,
 }) => {
+  console.log(country);
   return (
-    <h2>CountryInfo</h2>
-    // <CountryWrapper >
-    //   <Flag>
-    //     <Image />
-    //   </Flag>
-    //   <CountryDescription>
-    //     <CountryCapital>
-    //       Capital: <Accent></Accent>
-    //     </CountryCapital>
+    <CountryWrapper key={id}>
+      <Flag>
+        <Image src={flag} alt={country} />
+      </Flag>
+      <CountryDescription>
+        <CountryCapital>
+          Capital: <Accent>{capital}</Accent>
+        </CountryCapital>
 
-    //     <CountryTitle></CountryTitle>
+        <CountryTitle>{country}</CountryTitle>
 
-    //     <CountryDetail>
-    //       Population: <Accent></Accent>
-    //     </CountryDetail>
+        <CountryDetail>
+          Population: <Accent>{population}</Accent>
+        </CountryDetail>
 
-    //     <CountryDetail>
-    //       Languages: <Accent></Accent>
-    //     </CountryDetail>
-    //   </CountryDescription>
-    // </CountryWrapper>
+        <CountryDetail>
+          Languages: <Accent>{languages.join(', ')}</Accent>
+        </CountryDetail>
+      </CountryDescription>
+    </CountryWrapper>
   );
 };
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,48 @@
-import { Section, Container, CountryInfo, Loader } from 'components';
-
+import { Section, Container, CountryInfo, Loader, Heading } from 'components';
+import { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/country-service';
+import { Link } from 'react-router-dom';
 export const Country = () => {
+  const { countryId } = useParams();
+  const [country, setCountry] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const location = useLocation();
+  const backLink = location?.state?.from ?? '/';
+  useEffect(() => {
+    const countryInfo = async () => {
+      setIsLoading(true);
+      try {
+        const data = await fetchCountry(countryId);
+        setCountry(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    countryInfo();
+  }, [countryId]);
   return (
     <Section>
+      <div>
+        <Link to={backLink}>Go Back</Link>
+      </div>
       <Container>
-        <h2>Country</h2>
+        {isLoading && <Loader />}
+        {error ? (
+          <Heading>ERROR...{error}</Heading>
+        ) : (
+          <CountryInfo
+            flag={country.flag}
+            capital={country.capital}
+            country={country.countryName}
+            id={country.id}
+            languages={country.languages}
+            population={country.population}
+          />
+        )}
       </Container>
     </Section>
   );
